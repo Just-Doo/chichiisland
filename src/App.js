03fbe250128db9be8dd3e8d87ffd34cdd4536116
Just-Doo <engus200@naver.com>
2022-06-17 03:35:40 +0900

    page with a giraffe paiting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import './App.css';
+import Giraffe from './giraffe.jpg';
+import { Container, Navbar } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p> 
+      <header className="App-header" style={{
+        backgroundImage: `url(${Giraffe})`
+      }}>
+        <div>
+        <div style={{
+          backgroundColor : "white", opacity : "1"
+        }}>
+        <h1 style={{ 
+          color : "black", display : "inline", 
+          }}>
+            여기는 치치아일랜드입니다.
+        </h1>
+        </div>
+        </div>
+        <p>
+          <Navbar bg="dark" expand="sm" variant="dark">
+            <Container>
+              <Navbar.Brand href="#home" to='/'> 기린을 찾아 누르세요. </Navbar.Brand>
+            </Container>
+
+          </Navbar>
           <a
           className="App-link"
           href="https://cafe.naver.com/chichiisland"
           target="_blank"
           rel="noopener noreferrer"
           >
-          치치아일랜드
+          <img src={Giraffe} alt="It is a giraffe" width="300" />
           </a>
-          에 오신 것을 환영합니다.
+          <br/>기린, 사랑히시죠?
         </p>
-          이것은 제 껍니다.
       </header>
+      
     </div>
   );
 }
